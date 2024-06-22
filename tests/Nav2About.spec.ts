92a544dc7c5fa81bace4e1834dd8417ff1966cbc
studPe <158460492+studPe@users.noreply.github.com>
2024-06-22 14:15:24 +0200

    Improved PlayWright Test

--- tests/Nav2About.spec.ts
+++ tests/Nav2About.spec.ts
@@ -5,7 +5,8 @@ test('test', async ({ page }) => {
   await page.locator('#input-vaadin-number-field-14').click();
   await page.locator('#input-vaadin-number-field-14').fill('1234');
   await page.getByLabel('Select a name').click();
-  await page.getByRole('option', { name: 'Alice' }).click();
+  await page.getByRole('option', { name: 'Alice' }).locator('div').click();
   await page.getByRole('button', { name: 'Start Game!' }).click();
   await page.getByRole('button', { name: 'Close' }).click();
+  await page.getByRole('link', { name: 'About' }).click();
 });
--- tests/Playing_Game.spec.ts
+++ tests/Playing_Game.spec.ts
@@ -2,9 +2,24 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:8080/');
+  await page.locator('#input-vaadin-number-field-14').click();
+  await page.locator('#input-vaadin-number-field-14').fill('1234');
+  await page.getByLabel('Select a name').click();
+  await page.getByRole('option', { name: 'Bob' }).locator('div').click();
+  await page.getByRole('button', { name: 'Start Game!' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
   await page.getByLabel('Who you think you are').click();
-  await page.getByLabel('Who you think you are').fill('1234');
+  await page.getByLabel('Who you think you are').fill('way');
   await page.getByRole('button', { name: 'Enter Guess' }).click();
+  await page.getByLabel('Who you think you are').click();
+  await page.getByLabel('Who you think you are').fill('Meryl Can\'t-Streep-Online.');
   await page.getByRole('button', { name: 'Enter Guess' }).click();
-  await page.getByRole('button', { name: 'Enter Guess' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await page.getByRole('button', { name: 'New Game' }).click();
+  await page.locator('#input-vaadin-number-field-14').click();
+  await page.locator('#input-vaadin-number-field-14').fill('1234');
+  await page.getByLabel('Select a name').click();
+  await page.getByRole('option', { name: 'Alice' }).locator('div').click();
+  await page.getByRole('button', { name: 'Start Game!' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
 });
